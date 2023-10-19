e93b2b8206a8704ce55c524356786b1e2d8e4571
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-10-19 21:10:20 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ DEBIAN_FRONTEND=noninteractive
 # Clone Upstream
 git clone https://gitlab.gnome.org/GNOME/xdg-desktop-portal-gnome -b 45.0
 cp -rvf ./debian ./xdg-desktop-portal-gnome/
-cp -rvf ./patches/xdg-desktop-portal-gnome.service.in ./xdg-desktop-portal-gnome/data/
+cp -rf ./patches/xdg-desktop-portal-gnome.service.in ./xdg-desktop-portal-gnome/data/ || true
 cd ./xdg-desktop-portal-gnome/
 for i in $(cat ../patches/series) ; do echo "Applying Patch: $i" && patch -Np1 -i ../patches/$i || bash -c "echo "Applying Patch $i Failed!" && exit 2"; done
 
